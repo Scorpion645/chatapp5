9576c6b66a5ded90cc24488f0da143b058b846ca
Scorpion645 <scorpion645@gmail.com>
2022-11-28 12:11:09 -0500

    after video 84

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+late User signedInUser;
 final _firestore = FirebaseFirestore.instance;
 
 class ChatScreen extends StatefulWidget {
@@ -19,7 +20,6 @@ class _ChatScreenState extends State<ChatScreen> {
   String? messageText;
 
   final _auth = FirebaseAuth.instance;
-  late User signedInUser;
 
   @override
   void initState() {
@@ -131,8 +131,8 @@ class _ChatScreenState extends State<ChatScreen> {
 }
 
 class MessageLine extends StatelessWidget {
-  const MessageLine({super.key, required this.sender, required this.text});
-
+  const MessageLine({super.key, required this.sender, required this.text, required this.isMe});
+  final bool isMe;
   final String sender;
   final String text;
 
@@ -141,21 +141,25 @@ class MessageLine extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe? CrossAxisAlignment.end:CrossAxisAlignment.start,
         children: [
           Text(
             '$sender',
-            style: TextStyle(fontSize: 12, color: Colors.black45),
+            style: TextStyle(fontSize: 12, color: Colors.yellow[900]),
           ),
           Material(
             elevation: 5,
-            borderRadius: BorderRadius.circular(30),
-            color: Colors.blue[800],
+            borderRadius: BorderRadius.only(
+                topLeft: isMe? Radius.circular(30):Radius.circular(0),
+                topRight: isMe? Radius.circular(0):Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            color: isMe? Colors.blue[800]:Colors.white,
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               child: Text(
                 '$text',
-                style: TextStyle(fontSize: 15, color: Colors.white),
+                style: TextStyle(fontSize: 15, color: isMe? Colors.white:Colors.black45),
               ),
             ),
           ),
@@ -186,9 +190,11 @@ class MessageStreamBuilder extends StatelessWidget {
         for (var message in messages) {
           final messageText = message.get('text');
           final messageSender = message.get('sender');
+          final currentUser = signedInUser.email;
+
           final messageWidget = MessageLine(
             sender: messageSender,
-            text: messageText,
+            text: messageText, isMe: currentUser == messageSender? true:false,
           );
           messageWidgets.add(messageWidget);
         }
