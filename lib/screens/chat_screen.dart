da21164c93b63a1ec5362d307aea1f70961e2896
Scorpion645 <scorpion645@gmail.com>
2022-11-27 20:18:42 -0500

    after video 82

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -86,7 +86,29 @@ class _ChatScreenState extends State<ChatScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(),
+          StreamBuilder<QuerySnapshot>(
+            stream: _firestore.collection('messages').snapshots(),
+            builder: (context, snapshot) {
+              List<Text> messageWidgets = [];
+
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.blue,
+                  ),
+                );
+              }
+              final messages = snapshot.data!.docs;
+              for (var message in messages) {
+                final messageText = message.get('text');
+                final messageSender = message.get('sender');
+                final messageWidget = Text('$messageText - $messageSender');
+                messageWidgets.add(messageWidget);
+              }
+
+              return Column(children: messageWidgets);
+            },
+          ),
           Container(
             decoration: BoxDecoration(
                 border:
